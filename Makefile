c9368f5fe18a1c49fbe3254bb8041e146cc0e8ad
Thiago Cardoso <thiagoncc@gmail.com>
2011-02-04 23:19:16 -0200

    Added samples dir
    Now samples are going to be stored inside samples dir.

--- Makefile
+++ Makefile
@@ -5,8 +5,3 @@ CGOFILES=curses.go curses_defs.go
 CGO_LDFLAGS=-lncurses
 
 include $(GOROOT)/src/Make.pkg
-
-sample: *.go
-	$(GC) sample.go
-	$(LD) -o sample sample.$(O)
-
---
+++ samples/simple.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/thiagoncc/curses"
+
+const (
+windowHeight = 20
+windowWidth = 40
+)
+
+func main() {
+    curses.Initscr()
+    defer curses.End()
+    curses.Cbreak()
+    curses.Noecho()
+    curses.Stdscr.Keypad(true)
+
+    y,x := curses.Getmaxyx()
+    curses.Printw("Press any key to exit")
+    curses.Refresh()
+
+    window := curses.NewWindow(windowHeight,windowWidth,(y-windowHeight)/2,(x-windowWidth)/2)
+    window.Box(0,0)
+    window.Mvprintw(0,1,"Sample")
+    window.Refresh()
+
+    curses.Getch()
+}
